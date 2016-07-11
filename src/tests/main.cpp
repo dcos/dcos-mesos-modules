e3096286cf187ce88189e15144bc6bffbe8ae4a0
Avinash Sridharan <avinash@mesosphere.io>
2016-07-11 22:32:23 +0000

    Added a basic test for testing Master and Agent overlay modules.
    The test initializes the Master module and the Agent module. It
    verifies that the Master module is up by hitting the
    `overlay-master/state` endpoint. Further it compares the response from
    the Master and Agent `state` and `overlays` endpoints to make sure the
    overlays have been configured correctly.

---
+++ src/tests/main.cpp
@@ -0,0 +1,134 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// NOTE: This file is a direct copy of Mesos' `src/test/main.cpp` and
+// it needs to be kept in sync when it comes to fixes on the original.
+// We chose to not do an automated copy here as that might cause
+// confusing problems and also the original is rather stable.
+// TODO(tillt): Automate this.
+
+#include <string>
+
+#include <gtest/gtest.h>
+
+#include <process/process.hpp>
+
+#include <stout/flags.hpp>
+#include <stout/nothing.hpp>
+#include <stout/os.hpp>
+#include <stout/try.hpp>
+
+#include "logging/logging.hpp"
+
+#include "messages/messages.hpp" // For GOOGLE_PROTOBUF_VERIFY_VERSION.
+
+#include "module/manager.hpp"
+
+#include "tests/environment.hpp"
+#include "tests/flags.hpp"
+#include "tests/mesos.hpp"
+#include "tests/module.hpp"
+
+using namespace mesos::internal;
+using namespace mesos::internal::tests;
+
+using std::cerr;
+using std::cout;
+using std::endl;
+using std::string;
+
+
+int main(int argc, char** argv)
+{
+  GOOGLE_PROTOBUF_VERIFY_VERSION;
+
+  using mesos::internal::tests::flags; // Needed to disabmiguate.
+
+  // Load flags from environment and command line but allow unknown
+  // flags (since we might have gtest/gmock flags as well).
+  Try<flags::Warnings> load = flags.load("MESOS_", argc, argv, true);
+
+  if (load.isError()) {
+    cerr << flags.usage(load.error()) << endl;
+    return EXIT_FAILURE;
+  }
+
+  if (flags.help) {
+    cout << flags.usage() << endl;
+    testing::InitGoogleTest(&argc, argv); // Get usage from gtest too.
+    return EXIT_SUCCESS;
+  }
+
+  // Initialize Modules.
+  if (flags.modules.isSome() && flags.modulesDir.isSome()) {
+    EXIT(EXIT_FAILURE) <<
+      flags.usage("Only one of --modules or --modules_dir should be specified");
+  }
+
+  if (flags.modulesDir.isSome()) {
+    Try<Nothing> result =
+      mesos::modules::ModuleManager::load(flags.modulesDir.get());
+    if (result.isError()) {
+      EXIT(EXIT_FAILURE) << "Error loading modules: " << result.error();
+    }
+  }
+
+  Try<Nothing> result = tests::initModules(flags.modules);
+  if (result.isError()) {
+    cerr << "Error initializing modules: " << result.error() << endl;
+    return EXIT_FAILURE;
+  }
+
+  // Disable /metrics/snapshot rate limiting, but do not
+  // overwrite whatever the user set.
+  os::setenv("LIBPROCESS_METRICS_SNAPSHOT_ENDPOINT_RATE_LIMIT", "", false);
+
+  // If `process::initialize()` returns `false`, then it was called before this
+  // invocation, meaning the authentication realm for libprocess-level HTTP
+  // endpoints was set incorrectly. This should be the first invocation.
+  if (!process::initialize(None(), DEFAULT_HTTP_AUTHENTICATION_REALM)) {
+    EXIT(EXIT_FAILURE) << "The call to `process::initialize()` in the tests' "
+                       << "`main()` was not the function's first invocation";
+  }
+
+  // Be quiet by default!
+  if (!flags.verbose) {
+    flags.quiet = true;
+  }
+
+  // Initialize logging.
+  logging::initialize(argv[0], flags, true);
+
+  // Log any flag warnings (after logging is initialized).
+  foreach (const flags::Warning& warning, load->warnings) {
+    LOG(WARNING) << warning.message;
+  }
+
+  // Initialize gmock/gtest.
+  testing::InitGoogleTest(&argc, argv);
+  testing::FLAGS_gtest_death_test_style = "threadsafe";
+
+  cout << "Source directory: " << flags.source_dir << endl;
+  cout << "Build directory: " << flags.build_dir << endl;
+
+  // Instantiate our environment. Note that it will be managed by
+  // gtest after we add it via testing::AddGlobalTestEnvironment.
+  environment = new Environment(flags);
+
+  testing::AddGlobalTestEnvironment(environment);
+
+  return RUN_ALL_TESTS();
+}
---
+++ src/tests/overlay_tests.cpp
@@ -0,0 +1,395 @@
+/* file is © 2016 Mesosphere, Inc. (“Mesosphere”).  Mesosphere licenses
+ * this file to you solely pursuant to the following terms (and you may not use
+ * this file except in compliance with such terms):
+ *
+ * 1. Subject to your compliance with the following terms, Mesosphere hereby
+ * grants you a nonexclusive, limited, personal, non-sublicensable,
+ * non-transferable, royalty-free license to use this file solely for your
+ * internal business purposes.
+ *
+ * 2. You may not (and agree not to, and not to authorize or enable others to),
+ * directly or indirectly: (a) copy, distribute, rent, lease, timeshare, operate
+ * a service bureau, or otherwise use for the benefit of a third party, this
+ * file; or (b) remove any proprietary notices from this file.  Except as
+ * expressly set forth herein, as between you and Mesosphere, Mesosphere retains
+ * all right, title and interest in and to this file.
+ *
+ * 3. Unless required by applicable law or otherwise agreed to in writing,
+ * Mesosphere provides this file on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, either express or implied, including, without
+ * limitation, any warranties or conditions of TITLE, NON-INFRINGEMENT,
+ * MERCHANTABILITY, or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * 4. In no event and under no legal theory, whether in tort (including
+ * negligence), contract, or otherwise, unless required by applicable law (such
+ * as deliberate and grossly negligent acts) or agreed to in writing, shall
+ * Mesosphere be liable to you for damages, including any direct, indirect,
+ * special, incidental, or consequential damages of any character arising as a
+ * result of these terms or out of the use or inability to use this file
+ * (including but not limited to damages for loss of goodwill, work stoppage,
+ * computer failure or malfunction, or any and all other commercial damages or
+ * losses), even if Mesosphere has been advised of the possibility of such
+ * damages.
+ */
+
+#include <string>
+#include <ostream>
+
+#include <gmock/gmock.h>
+
+#include <mesos/hook.hpp>
+#include <mesos/mesos.hpp>
+#include <mesos/resources.hpp>
+
+#include <mesos/module/module.hpp>
+#include <mesos/module/anonymous.hpp>
+
+#include <mesos/scheduler/scheduler.hpp>
+
+#include <mesos/slave/isolator.hpp>
+
+#include <process/future.hpp>
+#include <process/gmock.hpp>
+#include <process/gtest.hpp>
+#include <process/http.hpp>
+#include <process/process.hpp>
+#include <process/owned.hpp>
+
+#include <stout/gtest.hpp>
+#include <stout/json.hpp>
+#include <stout/option.hpp>
+#include <stout/os.hpp>
+#include <stout/path.hpp>
+#include <stout/protobuf.hpp>
+#include <stout/try.hpp>
+
+#include <stout/os/read.hpp>
+
+#include "hook/manager.hpp"
+
+#include "master/detector/standalone.hpp"
+
+#include "module/manager.hpp"
+
+#include "overlay/overlay.hpp"
+#include "overlay/overlay.pb.h"
+#include "overlay/internal/messages.pb.h"
+
+#include "slave/flags.hpp"
+
+#include "tests/mesos.hpp"
+
+using namespace process;
+
+using namespace mesos::internal::tests;
+
+using std::cout;
+using std::endl;
+
+using process::Future;
+using process::Owned;
+using process::PID;
+using process::UPID;
+
+using process::http::OK;
+using process::http::Response;
+
+using mesos::internal::master::Master;
+
+using mesos::internal::slave::Containerizer;
+using mesos::internal::slave::MesosContainerizerProcess;
+using mesos::internal::slave::Slave;
+
+using mesos::master::detector::MasterDetector;
+
+using mesos::modules::Anonymous;
+using mesos::modules::ModuleManager;
+using mesos::modules::overlay::AgentInfo;
+using mesos::modules::overlay::AGENT_MANAGER_PROCESS_ID;
+using mesos::modules::overlay::MASTER_MANAGER_PROCESS_ID;
+using mesos::modules::overlay::internal::AgentConfig;
+using mesos::modules::overlay::internal::AgentRegisteredMessage;
+using mesos::modules::overlay::internal::MasterConfig;
+using mesos::modules::overlay::OverlayInfo;
+using mesos::modules::overlay::State;
+
+namespace mesos {
+namespace overlay {
+namespace tests {
+
+constexpr char OVERLAY_TEST_BASE_DIR[] = "/tmp/overlay-test/";
+constexpr char MASTER_JSON_CONFIG[] = "master.json";
+constexpr char AGENT_JSON_CONFIG[] = "agent.json";
+constexpr char AGENT_CNI_DIR[] = "cni/";
+constexpr char MASTER_OVERLAY_MODULE_NAME[] =
+  "com_mesosphere_mesos_OverlayMasterManager";
+constexpr char AGENT_OVERLAY_MODULE_NAME[] =
+  "com_mesosphere_mesos_OverlayAgentManager";
+
+class OverlayTest : public MesosTest 
+{ 
+protected:
+  virtual void SetUp()
+  {
+    MesosTest::SetUp();
+
+    auto modulesConfig = [](JSON::ObjectWriter* writer) {
+      writer->field("libraries", [](JSON::ArrayWriter* writer) {
+          writer->element([](JSON::ObjectWriter* writer) {
+            writer->field(
+              "file",
+              path::join(MODULES_BUILD_DIR,
+                         ".libs/libmesos_network_overlay.so"));
+            writer->field("modules", [](JSON::ArrayWriter* writer) {
+              auto masterModuleConfig = [](JSON::ObjectWriter* writer) {
+              writer->field("name", "com_mesosphere_mesos_OverlayMasterManager");
+              writer->field("parameters", [](JSON::ArrayWriter* writer) {
+                writer->element([](JSON::ObjectWriter* writer) {
+                  writer->field("key", "master_config");
+                  writer->field("value",
+                    path::join(OVERLAY_TEST_BASE_DIR, MASTER_JSON_CONFIG));
+                  });
+                });
+              };
+
+              auto agentModuleConfig = [](JSON::ObjectWriter* writer) {
+              writer->field("name", "com_mesosphere_mesos_OverlayAgentManager");
+              writer->field("parameters", [](JSON::ArrayWriter* writer) {
+                writer->element([](JSON::ObjectWriter* writer) {
+                  writer->field("key", "agent_config");
+                  writer->field("value",
+                    path::join(OVERLAY_TEST_BASE_DIR, AGENT_JSON_CONFIG));
+                  });
+                });
+              };
+
+              writer->element(masterModuleConfig);
+              writer->element(agentModuleConfig);
+            });
+          });
+      });
+    };
+
+    Try<JSON::Object> json = JSON::parse<JSON::Object>(jsonify(modulesConfig));
+    ASSERT_SOME(json);
+
+    Try<Modules> _modules = protobuf::parse<Modules>(json.get());
+    ASSERT_SOME(_modules);
+
+    modules = _modules.get();
+
+    // Initialize the modules.
+    Try<Nothing> result = ModuleManager::load(modules);
+    ASSERT_SOME(result);
+  }
+
+  virtual void TearDown()
+  {
+    LOG(INFO) << "Unloading all modules...";
+    // Unload all modules.
+    foreach (const Modules::Library& library, modules.libraries()) {
+      foreach (const Modules::Library::Module& module, library.modules()) {
+        if (module.has_name()) {
+          ASSERT_SOME(ModuleManager::unload(module.name()));
+        }
+      }
+    }
+
+    // Clean all the directories that we created.
+    os::rmdir(OVERLAY_TEST_BASE_DIR);
+  }
+
+  void startOverlayMaster(const MasterConfig& masterOverlayConfig)
+  {
+    Try<Nothing> mkdir = os::mkdir(OVERLAY_TEST_BASE_DIR);
+    EXPECT_SOME(mkdir);
+
+    Try<Nothing> write = os::write(
+        path::join(OVERLAY_TEST_BASE_DIR, MASTER_JSON_CONFIG),
+        stringify(JSON::protobuf(masterOverlayConfig)));
+    EXPECT_SOME(write);
+
+    Try<Anonymous*> create = ModuleManager::create<Anonymous>(
+        MASTER_OVERLAY_MODULE_NAME);
+    ASSERT_SOME(create);
+
+    masterOverlayModule = Owned<Anonymous>(create.get());
+  }
+
+  void startOverlayAgent(const AgentConfig& agentOverlayConfig)
+  {
+    Try<Nothing> mkdir = os::mkdir(Path(AGENT_JSON_CONFIG).dirname());
+    EXPECT_SOME(mkdir);
+
+    Try<Nothing> write = os::write(
+        path::join(OVERLAY_TEST_BASE_DIR, AGENT_JSON_CONFIG),
+        stringify(JSON::protobuf(agentOverlayConfig)));
+    EXPECT_SOME(write);
+
+    Try<Anonymous*> create = ModuleManager::create<Anonymous>(
+        AGENT_OVERLAY_MODULE_NAME);
+    ASSERT_SOME(create);
+
+    agentOverlayModule = Owned<Anonymous>(create.get());
+  }
+
+  Try<State> parseMasterState(const string& state)
+  {
+    Try<JSON::Object> json = JSON::parse<JSON::Object>(state);
+    if (json.isError()) {
+      return Error("JSON parse failed: " + json.error());
+    }
+
+    Try<State> parse = ::protobuf::parse<State>(json.get());
+
+    return parse;
+  }
+
+  Try<AgentInfo> parseAgentOverlay(const string& info)
+  {
+    Try<JSON::Object> json = JSON::parse<JSON::Object>(info);
+    if (json.isError()) {
+      return Error("JSON parse failed: " + json.error());
+    }
+
+    Try<AgentInfo> parse = ::protobuf::parse<AgentInfo>(json.get());
+
+    return parse;
+  }
+
+private:
+  Modules modules;
+
+  Owned<Anonymous> masterOverlayModule;
+  Owned<Anonymous> agentOverlayModule;
+};
+
+
+// Tests the ability of the `Master overlay module` to allocate
+// subnets to Agents for a given overlay network.
+TEST_F(OverlayTest, checkMasterAgentComm)
+{
+  Try<Owned<cluster::Master>> master = StartMaster();
+  ASSERT_SOME(master);
+
+  LOG(INFO) << "Master PID: " << master.get()->pid;
+
+  MasterConfig masterOverlayConfig;
+  masterOverlayConfig.mutable_network()->set_vtep_subnet("44.128.0.0/16");
+  masterOverlayConfig.mutable_network()->set_vtep_mac_oui("70:B3:D5:00:00:00");
+
+  OverlayInfo overlay;
+  overlay.set_name("overlay-1");
+  overlay.set_subnet("192.168.0.0/16");
+  overlay.set_prefix(24);
+
+  masterOverlayConfig.mutable_network()->add_overlays()->CopyFrom(overlay);
+
+  startOverlayMaster(masterOverlayConfig);
+
+  // Master `Anonymous` module created successfully. Lets see if we
+  // can hit the `state` endpoint of the Master.
+  UPID overlayMaster = UPID(master.get()->pid);
+  overlayMaster.id = MASTER_MANAGER_PROCESS_ID;
+
+  Future<Response> masterResponse = process::http::get(
+      overlayMaster,
+      "state");
+
+  AWAIT_EXPECT_RESPONSE_STATUS_EQ(OK().status, masterResponse);
+  AWAIT_EXPECT_RESPONSE_HEADER_EQ(
+      APPLICATION_JSON,
+      "Content-Type",
+      masterResponse);
+      
+  // The `state` end-point is backed by the
+  // mesos::modules::overlay::State protobuf, so need to parse the
+  // JSON and verify that the correct configuration is being
+  // reflected.
+  Try<State> state = parseMasterState(masterResponse->body);
+  ASSERT_SOME(state);
+  ASSERT_EQ(1, state->network().overlays_size());
+  ASSERT_EQ("overlay-1", state->network().overlays(0).name());
+  ASSERT_EQ("192.168.0.0/16", state->network().overlays(0).subnet());
+  ASSERT_EQ(24, state->network().overlays(0).prefix());
+
+  // We haven't started the Agent, so make sure there are no Agents
+  // reflected at this end-point.
+  ASSERT_EQ(0, state->agents_size());
+
+  AgentConfig agentOverlayConfig;
+  agentOverlayConfig.set_master(stringify(overlayMaster.address));
+  agentOverlayConfig.set_cni_dir(AGENT_CNI_DIR);
+  agentOverlayConfig.mutable_network_config()->set_allocate_subnet(true);
+  agentOverlayConfig.mutable_network_config()->set_mesos_bridge(false);
+  agentOverlayConfig.mutable_network_config()->set_docker_bridge(false);
+
+  // Setup a future to notify the test that Agent overlay module has
+  // registered.
+  Future<AgentRegisteredMessage> agentRegisteredMessage = 
+    FUTURE_PROTOBUF(AgentRegisteredMessage(), _, _);
+
+  startOverlayAgent(agentOverlayConfig);
+
+  AWAIT_READY(agentRegisteredMessage);
+
+  // Agent manager has been created. Hit the `overlay` endpoint to
+  // check that module is up and responding.
+  UPID overlayAgent = UPID(master.get()->pid);
+  overlayAgent.id = AGENT_MANAGER_PROCESS_ID;
+
+   Future<Response> agentResponse = process::http::get(
+      overlayAgent,
+      "overlay");
+
+  AWAIT_EXPECT_RESPONSE_STATUS_EQ(OK().status, agentResponse);
+  AWAIT_EXPECT_RESPONSE_HEADER_EQ(
+      APPLICATION_JSON,
+      "Content-Type",
+      agentResponse);
+
+  // The `overlay` end-point is backed by the
+  // mesos::modules::overlay::AgentInfo protobuf, so need to parse the
+  // JSON and verify that the correct configuration is being
+  // reflected.
+  Try<AgentInfo> info = parseAgentOverlay(agentResponse->body);
+  ASSERT_SOME(info);
+
+  // There should be only 1 overlay.
+  ASSERT_EQ(1, info->overlays_size());
+  EXPECT_EQ("overlay-1", info->overlays(0).info().name());
+  
+  Try<net::IPNetwork> agentNetwork = net::IPNetwork::parse(
+      info->overlays(0).subnet(), AF_INET);
+  ASSERT_SOME(agentNetwork);
+  EXPECT_EQ(24, agentNetwork->prefix());
+  
+  Try<net::IPNetwork> allocatedSubnet = net::IPNetwork::parse(
+      "192.168.0.0/24", AF_INET); 
+  ASSERT_SOME(allocatedSubnet);
+  EXPECT_EQ(allocatedSubnet.get(), agentNetwork.get());
+
+  // Hit the `state` end-point again. We should be seeing the
+  // Agents overlay subnet allocation in the `state` endpoint.
+  masterResponse = process::http::get(
+      overlayMaster,
+      "state");
+
+  AWAIT_EXPECT_RESPONSE_STATUS_EQ(OK().status, masterResponse);
+  AWAIT_EXPECT_RESPONSE_HEADER_EQ(
+      APPLICATION_JSON,
+      "Content-Type",
+      masterResponse);
+  state = parseMasterState(masterResponse->body);
+  ASSERT_SOME(state);
+  ASSERT_EQ(1, state->agents_size());
+
+  AgentInfo masterAgentInfo;
+  masterAgentInfo.CopyFrom(state->agents(0));
+  EXPECT_TRUE(
+      info.get().SerializeAsString() == masterAgentInfo.SerializeAsString());
+}
+} // namespace tests {
+} // namespace overlay {
+} // namespace mesos {
+
