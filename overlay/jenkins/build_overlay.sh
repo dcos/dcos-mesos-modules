a86829e474e6cbb2728e3dbb8d4e1dc0690ca93c
Joseph Wu <josephwu@apache.org>
2016-10-19 12:12:21 -0700

    Moved 'docker' folder into 'overlay/jenkins'.
    These build helpers need revisions:
    * Un-pin the build from a specific Mesos tarball.
    * Change the base Docker image to a DC/OS-supported platform.
    * Add a README.

--- overlay/jenkins/build_overlay.sh
+++ overlay/jenkins/build_overlay.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
-git clone https://github.com/dcos/mesos-overlay-modules.git /mesos-overlay-modules
-cd /mesos-overlay-modules
+
+git clone https://github.com/dcos/dcos-mesos-modules.git /mesos-mesos-modules
+cd /dcos-mesos-modules
 git fetch origin pull/$1/head:unit-tests
 git checkout unit-tests
 mkdir build
