f231f33260003b0c4166a1690d15c5f9cd7ce881
Joseph Wu <josephwu@apache.org>
2017-01-09 18:43:25 -0800

    Renamed a variety of things which became ambiguous due to merging the loggers.
    Mostly renames variables so that they specify whether they are for the
    logrotate bits or the journald bits.  Also clarified a few comments.

--- journald/lib_journald.hpp
+++ journald/lib_journald.hpp
@@ -30,18 +30,18 @@ struct LoggerFlags : public virtual flags::FlagsBase
     add(&LoggerFlags::destination_type,
         "destination_type",
         "Determines where logs should be piped.\n"
-        "Valid destinations include: 'journald', 'sandbox', or 'both'.",
+        "Valid destinations include: 'journald', 'logrotate', or 'both'.",
         "journald",
         [](const std::string& value) -> Option<Error> {
-          if (value != "journald" && value != "sandbox" && value != "both") {
+          if (value != "journald" && value != "logrotate" && value != "both") {
             return Error("Invalid destination type: " + value);
           }
 
           return None();
         });
 
-    add(&LoggerFlags::max_stdout_size,
-        "max_stdout_size",
+    add(&LoggerFlags::logrotate_max_stdout_size,
+        "logrotate_max_stdout_size",
         "Maximum size, in bytes, of a single stdout log file.\n"
         "Defaults to 10 MB.  Must be at least 1 (memory) page.",
         Megabytes(10),
@@ -54,12 +54,12 @@ struct LoggerFlags : public virtual flags::FlagsBase
         "i.e.\n"
         "  /path/to/stdout {\n"
         "    <logrotate_stdout_options>\n"
-        "    size <max_stdout_size>\n"
+        "    size <logrotate_max_stdout_size>\n"
         "  }\n"
-        "NOTE: The 'size' option will be overriden by this module.");
+        "NOTE: The 'size' option will be overridden by this module.");
 
-    add(&LoggerFlags::max_stderr_size,
-        "max_stderr_size",
+    add(&LoggerFlags::logrotate_max_stderr_size,
+        "logrotate_max_stderr_size",
         "Maximum size, in bytes, of a single stderr log file.\n"
         "Defaults to 10 MB.  Must be at least 1 (memory) page.",
         Megabytes(10),
@@ -72,9 +72,9 @@ struct LoggerFlags : public virtual flags::FlagsBase
         "i.e.\n"
         "  /path/to/stderr {\n"
         "    <logrotate_stderr_options>\n"
-        "    size <max_stderr_size>\n"
+        "    size <logrotate_max_stderr_size>\n"
         "  }\n"
-        "NOTE: The 'size' option will be overriden by this module.");
+        "NOTE: The 'size' option will be overridden by this module.");
   }
 
   static Option<Error> validateSize(const Bytes& value)
@@ -90,10 +90,10 @@ struct LoggerFlags : public virtual flags::FlagsBase
 
   std::string destination_type;
 
-  Bytes max_stdout_size;
+  Bytes logrotate_max_stdout_size;
   Option<std::string> logrotate_stdout_options;
 
-  Bytes max_stderr_size;
+  Bytes logrotate_max_stderr_size;
   Option<std::string> logrotate_stderr_options;
 };
 
@@ -105,15 +105,15 @@ struct Flags : public virtual LoggerFlags
     add(&Flags::environment_variable_prefix,
         "environment_variable_prefix",
         "Prefix for environment variables meant to modify the behavior of\n"
-        "the logrotate logger for the specific executor being launched.\n"
-        "The logger will look for four prefixed environment variables in the\n"
+        "the container logger for the specific executor being launched.\n"
+        "The logger will look for the prefixed environment variables in the\n"
         "'ExecutorInfo's 'CommandInfo's 'Environment':\n"
         "  * DESTINATION_TYPE\n"
-        "  * MAX_STDOUT_SIZE\n"
+        "  * LOGROTATE_MAX_STDOUT_SIZE\n"
         "  * LOGROTATE_STDOUT_OPTIONS\n"
-        "  * MAX_STDERR_SIZE\n"
+        "  * LOGROTATE_MAX_STDERR_SIZE\n"
         "  * LOGROTATE_STDERR_OPTIONS\n"
-        "If present, these variables will overwrite the global values set\n"
+        "If present, these variables will override the global values set\n"
         "via module parameters.",
         "CONTAINER_LOGGER_");
 
@@ -179,18 +179,21 @@ struct Flags : public virtual LoggerFlags
 };
 
 
-// The `JournaldContainerLogger` is a ContainerLogger module that pipes
-// the stdout/stderr of a container to journald. The module assumes the
-// stdout/stderr outputs generic single line logs (i.e. delineated by
-// newlines) and adds identifying labels to each log line; such
-// as the FrameworkID, ExecutorID, ContainerID, and labels inside
-// ExecutorInfo.
-//
 // For backwards compatibility, this module provides three modes:
 //   * Log to journald.
 //   * Log to journald and the sandbox, with log rotation.
 //   * Log to sandbox, with log rotation.
-// Logging to sandbox is equivalent to using the `LogrotateContainerLogger`.
+//
+// When logging to journald, the `JournaldContainerLogger` is a
+// ContainerLogger module that pipes the stdout/stderr of a container
+// to journald. The module assumes the stdout/stderr outputs generic
+// single line logs (i.e. delineated by newlines) and adds identifying
+// labels to each log line; such as the FrameworkID, ExecutorID,
+// ContainerID, and labels inside ExecutorInfo.
+//
+// Logging to sandbox is approximately equivalent to using the
+// `LogrotateContainerLogger`, packaged with vanilla Mesos.
+// See `Flags` above.
 class JournaldContainerLogger : public mesos::slave::ContainerLogger
 {
 public:
