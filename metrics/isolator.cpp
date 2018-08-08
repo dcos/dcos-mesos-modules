c8f66c48270e9d10d5084ca10b165f966fac5a07
Kevin Klues <klueska@gmail.com>
2018-08-08 06:27:24 -0400

    Added barebones implementation of metrics isolator module.

---
+++ metrics/isolator.cpp
@@ -0,0 +1,209 @@
+#include <map>
+#include <string>
+#include <vector>
+
+#include <mesos/mesos.hpp>
+#include <mesos/module.hpp>
+
+#include <mesos/module/isolator.hpp>
+#include <mesos/module/module.hpp>
+
+#include <process/address.hpp>
+#include <process/defer.hpp>
+#include <process/future.hpp>
+#include <process/process.hpp>
+
+#include <stout/assert.hpp>
+#include <stout/hashset.hpp>
+#include <stout/ip.hpp>
+#include <stout/nothing.hpp>
+#include <stout/numify.hpp>
+#include <stout/option.hpp>
+#include <stout/os.hpp>
+#include <stout/path.hpp>
+#include <stout/strings.hpp>
+#include <stout/try.hpp>
+
+#include "isolator.hpp"
+
+#include "metrics/messages.pb.h"
+
+namespace inet = process::network::inet;
+namespace unix = process::network::unix;
+
+using namespace mesos;
+using namespace process;
+
+using std::map;
+using std::string;
+using std::vector;
+
+using mesos::slave::ContainerLaunchInfo;
+using mesos::slave::ContainerState;
+using mesos::slave::Isolator;
+
+namespace mesosphere {
+namespace dcos {
+namespace metrics {
+
+class MetricsIsolatorProcess
+  : public process::Process<MetricsIsolatorProcess>
+{
+public:
+  MetricsIsolatorProcess(const isolator::Flags& _flags) : flags(_flags)
+  {
+    // Set `serviceScheme` based on flags.
+    ASSERT(flags.service_scheme.isSome());
+    ASSERT(flags.service_scheme.get() == "http" ||
+           flags.service_scheme.get() == "https");
+    serviceScheme = flags.service_scheme.get();
+
+    // Set `service*Address` based on flags.
+    ASSERT(flags.service_address.isSome());
+
+    ASSERT(flags.service_network.isSome());
+    ASSERT(flags.service_network.get() == "inet" ||
+           flags.service_network.get() == "unix");
+
+    if (flags.service_network.get() == "inet") {
+      vector<string> hostport =
+        strings::split(flags.service_address.get(), ":");
+      if (hostport.size() != 2) {
+        LOG(FATAL) << "Unable to split '" << flags.service_address.get() << "'"
+                   << " into valid 'host:port' combination";
+      }
+
+      Try<net::IP> ip = net::IP::parse(hostport[0]);
+      if (ip.isError()) {
+        LOG(FATAL) << "Unable to parse '" << hostport[0] << "'"
+                   << " as a valid IP address: " << ip.error();
+      }
+
+      Try<uint16_t> port = numify<uint16_t>(hostport[1]);
+      if (port.isError()) {
+        LOG(FATAL) << "Unable parse '" + hostport[1] + "'"
+                   << " as a valid port of type 'uint16_t': " + port.error();
+      }
+
+      serviceInetAddress = inet::Address(ip.get(), port.get());
+    }
+
+    if (flags.service_network.get() == "unix") {
+      Try<unix::Address> address =
+        unix::Address::create(flags.service_address.get());
+      if (address.isError()) {
+        LOG(FATAL) << "Unable to convert '" + flags.service_address.get() + "'"
+                   << " to valid 'unix' address: " + address.error();
+      }
+
+      serviceUnixAddress = address.get();
+    }
+
+    // Set `serviceEndpoint` based on flags.
+    ASSERT(flags.service_endpoint.isSome());
+    serviceEndpoint = flags.service_endpoint.get();
+  }
+
+  // Let the metrics service know about the container being launched.
+  // In the response, grab the STATSD_UDP_HOST and STATSD_UDP_PORT
+  // pair being returned and set it in the environment of the
+  // `ContainerLaunchInfo` returned from this function.
+  virtual Future<Option<ContainerLaunchInfo>> prepare(
+      const ContainerID& containerId,
+      const slave::ContainerConfig& containerConfig)
+  {
+    // Let the metrics service know about the container being
+    // launched via an HTTP request. In the response, grab the
+    // STATSD_UDP_HOST and STATSD_UDP_PORT pair being returned and set
+    // it in the environment of the `ContainerLaunchInfo` returned
+    // from this function. On any errors, return a `Failure()`.
+    ContainerLaunchInfo launchInfo;
+    return launchInfo;
+  }
+
+  virtual Future<Nothing> cleanup(
+      const ContainerID& containerId)
+  {
+    // Let the metrics service know about the container being
+    // destroyed via an HTTP request. On any errors, return an
+    // `Failure()`.
+    return Nothing();
+  }
+
+private:
+  const isolator::Flags flags;
+  string serviceScheme;
+  string serviceEndpoint;
+  Option<inet::Address> serviceInetAddress;
+  Option<unix::Address> serviceUnixAddress;
+};
+
+
+MetricsIsolator::MetricsIsolator(const isolator::Flags& flags)
+  : process(new MetricsIsolatorProcess(flags))
+{
+  spawn(process.get());
+}
+
+
+MetricsIsolator::~MetricsIsolator()
+{
+  terminate(process.get());
+  wait(process.get());
+}
+
+
+Future<Option<ContainerLaunchInfo>> MetricsIsolator::prepare(
+    const ContainerID& containerId,
+    const slave::ContainerConfig& containerConfig)
+{
+  return dispatch(process.get(),
+                  &MetricsIsolatorProcess::prepare,
+                  containerId,
+                  containerConfig);
+}
+
+
+Future<Nothing> MetricsIsolator::cleanup(
+    const ContainerID& containerId)
+{
+  return dispatch(process.get(),
+                  &MetricsIsolatorProcess::cleanup,
+                  containerId);
+}
+
+} // namespace metrics {
+} // namespace dcos {
+} // namespace mesosphere {
+
+mesos::modules::Module<Isolator>
+com_mesosphere_dcos_MetricsIsolatorModule(
+    MESOS_MODULE_API_VERSION,
+    MESOS_VERSION,
+    "Mesosphere",
+    "support@mesosphere.com",
+    "Metrics Isolator Module.",
+    nullptr,
+    [](const Parameters& parameters) -> Isolator* {
+      // Convert `parameters` into a map.
+      map<string, string> values;
+      foreach (const Parameter& parameter, parameters.parameter()) {
+        values[parameter.key()] = parameter.value();
+      }
+
+      mesosphere::dcos::metrics::isolator::Flags flags;
+
+      // Load and validate flags.
+      Try<flags::Warnings> load = flags.load(values, false);
+
+      if (load.isError()) {
+        LOG(ERROR) << "Failed to parse parameters: " << load.error();
+        return nullptr;
+      }
+
+      foreach (const flags::Warning& warning, load->warnings) {
+        LOG(WARNING) << warning.message;
+      }
+
+      return new mesosphere::dcos::metrics::MetricsIsolator(flags);
+    });
---
+++ metrics/isolator.hpp
@@ -0,0 +1,142 @@
+#ifndef __METRICS_ISOLATOR_MODULE_HPP__
+#define __METRICS_ISOLATOR_MODULE_HPP__
+
+#include <map>
+#include <string>
+#include <vector>
+
+#include <mesos/mesos.hpp>
+#include <mesos/module.hpp>
+
+#include <mesos/slave/isolator.hpp>
+
+#include <process/future.hpp>
+#include <process/owned.hpp>
+#include <process/process.hpp>
+
+#include <stout/duration.hpp>
+#include <stout/flags.hpp>
+#include <stout/hashset.hpp>
+#include <stout/option.hpp>
+#include <stout/try.hpp>
+
+namespace mesosphere {
+namespace dcos {
+namespace metrics {
+
+// Forward declaration.
+class MetricsIsolatorProcess;
+
+namespace isolator {
+
+struct Flags : virtual flags::FlagsBase
+{
+  Flags()
+  {
+    add(&Flags::service_scheme,
+        "dcos_metrics_service_scheme",
+        "The scheme on which the --dcos_metrics_service_address is reachable.\n"
+        "Must be either \"http\" or \"https\".",
+        [](const Option<std::string>& value) -> Option<Error> {
+          if (value.isNone()) {
+            return Error("Missing required option"
+                         " --dcos_metrics_service_scheme");
+          }
+
+          if (value.get() != "http" && value.get() != "https") {
+            return Error("Expected --dcos_metrics_service_scheme"
+                         " to be either \"http\" or \"https\"");
+          }
+
+          return None();
+        });
+
+    add(&Flags::service_network,
+        "dcos_metrics_service_network",
+        "The network where the --dcos_metrics_service_address is reachable.\n"
+        "Must be either \"inet\" or \"unix\".",
+        [](const Option<std::string>& value) -> Option<Error> {
+          if (value.isNone()) {
+            return Error("Missing required option"
+                         " --dcos_metrics_service_network");
+          }
+
+          if (value.get() != "inet" && value.get() != "unix") {
+            return Error("Expected --dcos_metrics_service_network"
+                         " to be either \"inet\" or \"unix\"");
+          }
+
+          return None();
+        });
+
+    add(&Flags::service_address,
+        "dcos_metrics_service_address",
+        "The address where the DC/OS metrics service is reachable.",
+        [](const Option<std::string>& value) -> Option<Error> {
+          if (value.isNone()) {
+            return Error("Missing required option"
+                         " --dcos_metrics_service_address");
+          }
+
+          return None();
+        });
+
+    add(&Flags::service_endpoint,
+        "dcos_metrics_service_endpoint",
+        "The endpoint on the --dcos_metrics_service_address that\n"
+        "this module should communicate with.",
+        [](const Option<std::string>& value) -> Option<Error> {
+          if (value.isNone()) {
+            return Error("Missing required option"
+                         " --dcos_metrics_service_endpoint");
+          }
+
+          return None();
+        });
+
+    add(&Flags::request_timeout,
+        "request_timeout",
+        "The duration after which a request to the metrics service will\n"
+        "timeout and be considered failed.",
+        Seconds(5));
+  }
+
+  // TODO(greggomann): Remove the `Option`s here once we have an overload of
+  // `add()` which accepts required flags without a default value and with a
+  // validation function.
+  Option<std::string> service_scheme;
+  Option<std::string> service_network;
+  Option<std::string> service_address;
+  Option<std::string> service_endpoint;
+  Duration request_timeout;
+};
+
+} // namespace isolator
+
+
+class MetricsIsolator : public mesos::slave::Isolator
+{
+public:
+  MetricsIsolator(const isolator::Flags& flags);
+
+  virtual ~MetricsIsolator();
+
+  virtual process::Future<Option<mesos::slave::ContainerLaunchInfo>> prepare(
+      const mesos::ContainerID& containerId,
+      const mesos::slave::ContainerConfig& containerConfig) override;
+
+  virtual process::Future<Nothing> cleanup(
+      const mesos::ContainerID& containerId) override;
+
+  virtual bool supportsNesting() override { return true; }
+  virtual bool supportsStandalone() override { return true; }
+
+private:
+  process::Owned<MetricsIsolatorProcess> process;
+};
+
+} // namespace metrics {
+} // namespace dcos {
+} // namespace mesosphere {
+
+#endif // __METRICS_ISOLATOR_MODULE_HPP__
