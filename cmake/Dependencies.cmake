7568242ecee93b3a4f9648d2e94dbedb617b5f28
Oleksandr Zasikan <Oleksandr_Zasikan@epam.com>
2020-02-21 20:37:08 +0200

    add mesos-tests and journald tests

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -10,6 +10,10 @@ set (ZOOKEEPER_INCL
   "${MESOS_ROOT}/build/3rdparty/zookeeper-${ZOOKEEPER_VERSION}/src/zookeeper-${ZOOKEEPER_VERSION}/src/c/generated"
   "${MESOS_ROOT}/build/3rdparty/zookeeper-${ZOOKEEPER_VERSION}/src/zookeeper-${ZOOKEEPER_VERSION}/src/c/include"
 )
+set (NVML_INCL "${MESOS_ROOT}/build/3rdparty/nvml-${NVML_VERSION}")
+set (GOOGLEMOCK_INCL "${MESOS_ROOT}/build/3rdparty/googletest-${GOOGLETEST_VERSION}/src/googletest-${GOOGLETEST_VERSION}/googlemock/include")
+set (GOOGLETEST_INCL "${MESOS_ROOT}/build/3rdparty/googletest-${GOOGLETEST_VERSION}/src/googletest-${GOOGLETEST_VERSION}/googletest/include")
+
 
 set (MESOS_BUILD_INCL "${MESOS_ROOT}/build/include")
 set (MESOS_INCL "${MESOS_ROOT}/include")
@@ -59,6 +63,19 @@ find_library(
 )
 
 find_library(
+  ZLIB_LIB_STATIC_DEBUG
+  NAMES zlibstaticd
+  PATHS  ${MESOS_ROOT}/build/3rdparty/zlib-${ZLIB_VERSION}/src/zlib-${ZLIB_VERSION}-build
+  PATH_SUFFIXES Debug
+)
+
+find_library(
+  ZLIB_LIB_STATIC_RELEASE
+  NAMES zlibstatic
+  PATHS  ${MESOS_ROOT}/build/3rdparty/zlib-${ZLIB_VERSION}/src/zlib-${ZLIB_VERSION}-build/Release
+)
+
+find_library(
   HTTP_PARSER_LIB
   NAMES http_parser
   PATHS ${MESOS_ROOT}/build/3rdparty/http_parser-${HTTP_PARSER_VERSION}/src/http_parser-${HTTP_PARSER_VERSION}-build
@@ -98,3 +115,67 @@ find_package(Protobuf CONFIG REQUIRED)
 
 
 find_package(OpenSSL REQUIRED)
+
+find_library(
+  GOOGLEMOCK_LIB_DEBUG
+  NAMES gmock
+  PATHS ${MESOS_ROOT}/build/3rdparty/googletest-1.8.0/src/googletest-${GOOGLETEST_VERSION}-build/googlemock/Debug
+)
+
+find_library(
+  GOOGLEMOCK_LIB_RELEASE
+  NAMES gmock
+  PATHS ${MESOS_ROOT}/build/3rdparty/googletest-1.8.0/src/googletest-${GOOGLETEST_VERSION}-build/googlemock/Release/
+)
+
+find_library(
+  GOOGLETEST_LIB_DEBUG
+  NAMES gtest
+  PATHS ${MESOS_ROOT}/build/3rdparty/googletest-1.8.0/src/googletest-${GOOGLETEST_VERSION}-build/googlemock/gtest/Debug
+)
+
+find_library(
+  GOOGLETEST_LIB_RELEASE
+  NAMES gtest
+  PATHS  ${MESOS_ROOT}/build/3rdparty/googletest-1.8.0/src/googletest-${GOOGLETEST_VERSION}-build/googlemock/gtest/Debug
+)
+
+find_library(
+  SASL_LIB_RELEASE
+  NAMES libsasl2
+  PATHS  ${MESOS_ROOT}/build/3rdparty/sasl2-2.1.27rc3/src/sasl2-2.1.27rc3-build/Release/
+)
+
+find_library(
+  SASL_LIB_DEBUG
+  NAMES libsasl2
+  PATHS  ${MESOS_ROOT}/build/3rdparty/sasl2-2.1.27rc3/src/sasl2-2.1.27rc3-build/Debug/
+)
+
+find_library(
+  BZIP2_LIB_DEBUG
+  NAMES bzip2.lib
+  PATHS  ${MESOS_ROOT}/build/3rdparty/bzip2-${BZIP2_VERSION}/src/bzip2-${BZIP2_VERSION}-build
+  PATH_SUFFIXES Debug
+)
+
+find_library(
+  BZIP2_LIB_RELEASE
+  NAMES bzip2.lib
+  PATHS  ${MESOS_ROOT}/build/3rdparty/bzip2-${BZIP2_VERSION}/src/bzip2-${BZIP2_VERSION}-build
+  PATH_SUFFIXES Release
+)
+
+find_library(
+  CURL_LIB_RELEASE
+  NAMES libcurl
+  PATHS  ${MESOS_ROOT}/build/3rdparty/curl-${CURL_VERSION}/src/curl-${CURL_VERSION}-build/lib/
+  PATH_SUFFIXES Release
+)
+
+find_library(
+  CURL_LIB_DEBUG
+  NAMES libcurl-d
+  PATHS  ${MESOS_ROOT}/build/3rdparty/curl-${CURL_VERSION}/src/curl-${CURL_VERSION}-build/lib
+  PATH_SUFFIXES Debug
+)
